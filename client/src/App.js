ce74c05098fce9a3bd386654636901ff1525bed7
Nikhil Nair <niks.nair@gmail.com>
2019-02-25 18:00:31 +0530

    front/back; no db

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+// client/src/App.js
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return <div>READY TO USE BACK-END APIS</div>;  
+  }
+}
+
+export default App;
